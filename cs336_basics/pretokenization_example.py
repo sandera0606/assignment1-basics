60cf35ef290b33f479463ce6e5588cfbe3fdd381
Sandra Huang <shuang2322@gmail.com>
2026-09-15 10:44:41 -0400

    starting with pretokenization

--- cs336_basics/pretokenization_example.py
+++ cs336_basics/pretokenization_example.py
@@ -50,13 +50,18 @@ def find_chunk_boundaries(
 
 
 ## Usage
-with open(..., "rb") as f:
-    num_processes = 4
-    boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
+# with open(..., "rb") as f:
+#     num_processes = 4
+#     boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
 
-    # The following is a serial implementation, but you can parallelize this
-    # by sending each start/end pair to a set of processes.
-    for start, end in zip(boundaries[:-1], boundaries[1:]):
-        f.seek(start)
-        chunk = f.read(end - start).decode("utf-8", errors="ignore")
+#     # The following is a serial implementation, but you can parallelize this
+#     # by sending each start/end pair to a set of processes.
+#     for start, end in zip(boundaries[:-1], boundaries[1:]):
+#         f.seek(start)
+#         chunk = f.read(end - start).decode("utf-8", errors="ignore")
         # Run pre-tokenization on your chunk and store the counts for each pre-token
+
+# pretokenization practice
+import regex as re
+PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+print(re.findall(PAT, "hello ni hao nihao nihowdy now nice how nice 你好。"))
---
+++ cs336_basics/tokenizer.py
@@ -0,0 +1,11 @@
+import cProfile
+# python -m cProfile -o profile.stats script.py
+# python -m pstats profile.stats
+# # Profile just this specific function call
+#    cProfile.run('my_function()')
+#    results = pstats.Stats(cProfile.Profile())
+#    results.sort_stats(pstats.SortKey.TIME)
+# can use tuna to visualize
+
+
+# py-spyrun -p $PID --duration 60 ??? --format raw
